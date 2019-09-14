dc4dafa5fafd7186786ac92a52cda6f5286d94c4
Caio Coelho <ccoelho6@uwo.ca>
2019-09-14 14:53:48 -0400

    Added html-python POST request integration

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder='assets')
 
 
 @app.route('/')
 def hello_world():
     return 'Hello Hack the North!'
 
-@app.route('/questions/N77G')
+
+@app.route('/questions/N77G', methods=['GET', 'POST'])
 def questions():
-    return 'Questions page!'
+    if request.method == 'POST':
+        asd = request.json
+        print(asd)
+        return render_template("index.html")
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()

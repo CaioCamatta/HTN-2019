7f0b13bdcf7a8ad7889a12a065b8ce0ad1761f15
Caio Coelho <ccoelho6@uwo.ca>
2019-09-14 23:23:05 -0400

    Connected first page

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def check_done(id):
 
 @app.route('/')
 def hello_world():
-    return 'Hello Hack the North!'
+        return render_template("intro.html")
 
 
 @app.route('/questions/N77G', methods=['GET', 'POST'])
@@ -60,7 +60,85 @@ def questions():
         return render_template("index.html")
     
     elif check_done(page_code):
-        return "DONE"
+        all_answers_given = [[],[],[],[],[],[],[],[],[],[]]
+​
+        participant_ref = db.collection(u'HackTheNorth').document(u"questions-N77G") 
+        actual_ref = db.collection(u'HackTheNorth').document(u"questions-N77G-correct")
+        
+        participant_doc = participant_ref.get()
+        actual_doc = actual_ref.get()
+        # print(participant_doc.to_dict())
+    ​
+        length_of_doc = len(actual_doc.to_dict()['answers'])
+        # print(participant_doc.to_dict()['answers_2'])
+    ​
+        answer_list = []
+    ​
+        correct_answers = [
+            actual_doc.to_dict()['answers']['1'],
+            actual_doc.to_dict()['answers']['2'], 
+            actual_doc.to_dict()['answers']['3'],
+            actual_doc.to_dict()['answers']['4'],
+            actual_doc.to_dict()['answers']['5'],
+            actual_doc.to_dict()['answers']['6'],
+            actual_doc.to_dict()['answers']['7'],
+            actual_doc.to_dict()['answers']['8'],
+            actual_doc.to_dict()['answers']['9'],
+            actual_doc.to_dict()['answers']['10']
+        ]
+    ​
+    ​
+        for key in participant_doc.to_dict().keys():
+            # print(key)
+            try:
+                if key[:6] == 'answer':
+                    answer_list.append(key)
+            except:
+                pass
+    ​
+        # print(answer_list)
+    ​
+        for key in answer_list: 
+    ​
+            guesses = [
+                participant_doc.to_dict()[key]['1'],
+                participant_doc.to_dict()[key]['2'], 
+                participant_doc.to_dict()[key]['3'],
+                participant_doc.to_dict()[key]['4'],
+                participant_doc.to_dict()[key]['5'],
+                participant_doc.to_dict()[key]['6'],
+                participant_doc.to_dict()[key]['7'],
+                participant_doc.to_dict()[key]['8'],
+                participant_doc.to_dict()[key]['9'],
+                participant_doc.to_dict()[key]['10']
+            ]
+    ​
+            
+        
+            for i in range(0, len(correct_answers)):
+                if guesses[i] == correct_answers[i]:
+                    all_answers_given[i].append('True')
+                else:
+                    all_answers_given[i].append('False')
+    ​
+        
+    ​
+        print(all_answers_given)
+    ​
+        individual_average_mark = 0
+        final_average_mark = []
+    ​
+        for i in all_answers_given:
+            for j in i:
+                if j == 'True':
+                    individual_average_mark += 1
+    ​
+        
+            final_average_mark.append(float(individual_average_mark)/len(i) *100)
+            individual_average_mark = 0
+    ​
+        print(final_average_mark)
+        return render_template("index.html", data=final_average_mark)
 
     return render_template("index.html")
 
